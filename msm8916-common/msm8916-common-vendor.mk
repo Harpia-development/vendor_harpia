b8ab9e0d080b95bbd23b1104bebdf721ac0bf13f
Kyle Harrison <khwebmail@gmail.com>
2018-05-11 12:29:22 +0100

    msm8916-common: Move GPS config files to device common
    Change-Id: Ie6dec4cd9047cb36e07b5efb013153943eb13a45

--- msm8916-common/msm8916-common-vendor.mk
+++ msm8916-common/msm8916-common-vendor.mk
@@ -46,9 +46,6 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/msm8916-common/proprietary/etc/firmware/venus.b04:system/etc/firmware/venus.b04 \
     vendor/motorola/msm8916-common/proprietary/etc/firmware/venus.mbn:system/etc/firmware/venus.mbn \
     vendor/motorola/msm8916-common/proprietary/etc/firmware/venus.mdt:system/etc/firmware/venus.mdt \
-    vendor/motorola/msm8916-common/proprietary/etc/flp.conf:system/etc/flp.conf \
-    vendor/motorola/msm8916-common/proprietary/etc/gps.conf:system/etc/gps.conf \
-    vendor/motorola/msm8916-common/proprietary/etc/izat.conf:system/etc/izat.conf \
     vendor/motorola/msm8916-common/proprietary/etc/permissions/qcnvitems.xml:system/etc/permissions/qcnvitems.xml \
     vendor/motorola/msm8916-common/proprietary/etc/permissions/qcrilhook.xml:system/etc/permissions/qcrilhook.xml \
     vendor/motorola/msm8916-common/proprietary/etc/sap.conf:system/etc/sap.conf \
