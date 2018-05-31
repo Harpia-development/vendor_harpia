7b2438646d2ac5cca73a5613c4bf3f005a9fb78a
althafvly <althafvly@gmail.com>
2018-05-31 10:43:06 +0000

    msm8916-common : Blobs update from aljeter OPP27.61-14-4
    -Update GPS blobs
    -Update Ril-blobs
    -Update Media blobs
    -Update Camera Blobs
    -Update Fm-radio blobs
    -Update AptX Blobs
    -Update IMS blobs
    -Regenerate Makefiles
    
    Change-Id: Id8f5faf7de119e5c00a5786bfa75703e2ed87ea0

--- msm8916-common/Android.mk
+++ msm8916-common/Android.mk
@@ -22,7 +22,7 @@ ifneq ($(filter harpia lux merlin osprey surnia,$(TARGET_DEVICE)),)
 include $(CLEAR_VARS)
 LOCAL_MODULE := vendor.qti.hardware.fm@1.0
 LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/lib/vendor.qti.hardware.fm@1.0.so
+LOCAL_SRC_FILES := proprietary/vendor/lib/vendor.qti.hardware.fm@1.0.so
 LOCAL_MULTILIB := 32
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -32,7 +32,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := TimeService
 LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/app/TimeService/TimeService.apk
+LOCAL_SRC_FILES := proprietary/vendor/app/TimeService/TimeService.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
