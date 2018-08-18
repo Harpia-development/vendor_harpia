996f7dfd3cb85137b2af8879ca1b2d778bfbf9d0
Akianonymus <anonymus.aki@gmail.com>
2018-08-18 08:30:44 +0530

    msm8916-common: Update IMS blobs
    from https://github.com/chil360/proprietary_vendor_motorola

--- msm8916-common/Android.mk
+++ msm8916-common/Android.mk
@@ -90,7 +90,7 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := imscmlibrary
 LOCAL_MODULE_OWNER := motorola
-LOCAL_SRC_FILES := proprietary/framework/imscmlibrary.jar
+LOCAL_SRC_FILES := proprietary/vendor/framework/imscmlibrary.jar
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := JAVA_LIBRARIES
