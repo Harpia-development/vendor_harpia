b3fdf3e5877414178e0658f15161b0ffb7980376
FacuM <facumo.fm@gmail.com>
2018-10-17 23:53:05 +0000

    msm8916: 32-bit perfd blob exec from athene.
    Change-Id: I46c087e6cfaf69e7e5b2413f6805301ba5e983e9

--- msm8916-common/msm8916-common-vendor.mk
+++ msm8916-common/msm8916-common-vendor.mk
@@ -169,7 +169,8 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/msm8916-common/proprietary/vendor/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so \
     vendor/motorola/msm8916-common/proprietary/vendor/lib/vendor.qti.hardware.fm@1.0_vendor.so:system/vendor/lib/vendor.qti.hardware.fm@1.0_vendor.so \
     vendor/motorola/msm8916-common/proprietary/vendor/qcril.db:system/vendor/qcril.db \
-    vendor/motorola/msm8916-common/proprietary/vendor/lib/libqti-perfd-client.so:system/vendor/lib/libqti-perfd-client.so
+    vendor/motorola/msm8916-common/proprietary/vendor/lib/libqti-perfd-client.so:system/vendor/lib/libqti-perfd-client.so \
+    vendor/motorola/msm8916-common/proprietary/vendor/bin/perfd:system/vendor/bin/perfd
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.fm@1.0 \
